2f7f7316b11fa9e29b6c5803b15408b43d89fdd0
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-12-20 22:37:38 +0100

    test: Prototype demo

---
+++ demo-shared/src/commonMain/kotlin/Api.kt
@@ -0,0 +1,53 @@
+package opensavvy.spine.demo
+
+import opensavvy.spine.typed.DynamicResource
+import opensavvy.spine.typed.ParameterStorage
+import opensavvy.spine.typed.Parameters
+import opensavvy.spine.typed.StaticResource
+
+object Api : StaticResource("v1", parent = null) {
+
+	object Users : StaticResource("users", Api) {
+
+		val get by get()
+			.parameters(UserDto::Params)
+			.response<List<Id>>()
+
+		val create by post()
+			.request<UserDto.New>()
+
+		val logIn by post("/token")
+			.request<UserDto.LogIn>()
+
+		object User : DynamicResource("user", Users) {
+
+			val get by get()
+				.response<UserDto>()
+
+			val edit by patch()
+				.request<UserDto.Edit>()
+
+		}
+	}
+}
+
+data class Id(val segments: List<String>)
+
+class UserDto {
+
+	object New
+
+	object LogIn
+
+	object Edit
+
+	class Params(
+		data: ParameterStorage,
+	) : Parameters(data) {
+		var archived by parameter("archived", false)
+	}
+}
+
+data class Foo(
+	val username: String,
+)
