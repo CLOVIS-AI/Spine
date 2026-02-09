d239491668e1d1f99e86b3fbfad44454776c03ce
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2026-02-09 23:40:41 +0100

    docs(client): Document the 'request' methods

--- client/src/commonMain/kotlin/Client.kt
+++ client/src/commonMain/kotlin/Client.kt
@@ -10,6 +10,48 @@ import opensavvy.spine.api.FailureSpec
 import opensavvy.spine.api.Parameters
 import opensavvy.spine.api.ResolvedEndpoint
 
+/**
+ * Invokes a [Ktor typesafe endpoint][ResolvedEndpoint].
+ *
+ * If the following API has been declared:
+ * ```kotlin
+ * object Api : RootResource("v1") {
+ *     object Users : StaticResource<Api>("users", Api) {
+ *         val list by get()
+ *             .response<List<User>>()
+ *
+ *         val create by post()
+ *             .request<UserCreation>()
+ *             .response<User>()
+ *
+ *         object User : DynamicResource<Users>("user", Users) {
+ *             val get by get()
+ *                  .response<User>()
+ *         }
+ *     }
+ * }
+ * ```
+ *
+ * An [HttpClient] can be used to call it:
+ *
+ * ```kotlin
+ * // List users:
+ * client.request(Api / Users / Users.list).bodyOrThrow()
+ *
+ * // Create a user:
+ * client.request(Api / Users / Users.create, UserCreation("John", 15)).bodyOrThrow()
+ *
+ * // Access a specific user:
+ * client.request(Api / Users / User("123456") / User.get).bodyOrThrow()
+ * ```
+ *
+ * For this example to work, you will need to configure the [HttpClient]'s `DefaultRequest` and `ContentNegotiation` plugin.
+ * To do so, please follow [our tutorial](https://spine.opensavvy.dev/setup.html#client-side-implementation).
+ *
+ * @see bodyOrNull Access the body, returning `null` on failure.
+ * @see bodyOrThrow Access the body, throwing an exception on failure.
+ * @see handle Exhaustively handle declared failures.
+ */
 suspend inline fun <reified In : Any, reified Out : Any, reified Failure : FailureSpec, reified Params : Parameters> HttpClient.request(
 	endpoint: ResolvedEndpoint<Endpoint<In, Out, Failure, Params>>,
 	input: In,
@@ -36,6 +78,48 @@ suspend inline fun <reified In : Any, reified Out : Any, reified Failure : Failu
 // The above was the real implementation.
 // However, most usage will be with the default body, etc.
 
+/**
+ * Invokes a [Ktor typesafe endpoint][ResolvedEndpoint].
+ *
+ * If the following API has been declared:
+ * ```kotlin
+ * object Api : RootResource("v1") {
+ *     object Users : StaticResource<Api>("users", Api) {
+ *         val list by get()
+ *             .response<List<User>>()
+ *
+ *         val create by post()
+ *             .request<UserCreation>()
+ *             .response<User>()
+ *
+ *         object User : DynamicResource<Users>("user", Users) {
+ *             val get by get()
+ *                  .response<User>()
+ *         }
+ *     }
+ * }
+ * ```
+ *
+ * An [HttpClient] can be used to call it:
+ *
+ * ```kotlin
+ * // List users:
+ * client.request(Api / Users / Users.list).bodyOrThrow()
+ *
+ * // Create a user:
+ * client.request(Api / Users / Users.create, UserCreation("John", 15)).bodyOrThrow()
+ *
+ * // Access a specific user:
+ * client.request(Api / Users / User("123456") / User.get).bodyOrThrow()
+ * ```
+ *
+ * For this example to work, you will need to configure the [HttpClient]'s `DefaultRequest` and `ContentNegotiation` plugin.
+ * To do so, please follow [our tutorial](https://spine.opensavvy.dev/setup.html#client-side-implementation).
+ *
+ * @see bodyOrNull Access the body, returning `null` on failure.
+ * @see bodyOrThrow Access the body, throwing an exception on failure.
+ * @see handle Exhaustively handle declared failures.
+ */
 suspend inline fun <reified Out : Any, reified Failure : FailureSpec, reified Params : Parameters> HttpClient.request(
 	endpoint: ResolvedEndpoint<Endpoint<Unit, Out, Failure, Params>>,
 	crossinline parameters: Params.() -> Unit,
@@ -43,6 +127,48 @@ suspend inline fun <reified Out : Any, reified Failure : FailureSpec, reified Pa
 	crossinline configure: HttpRequestBuilder.() -> Unit = {},
 ): SpineResponse<Out, Failure> = request(endpoint, Unit, parameters, contentType, configure)
 
+/**
+ * Invokes a [Ktor typesafe endpoint][ResolvedEndpoint].
+ *
+ * If the following API has been declared:
+ * ```kotlin
+ * object Api : RootResource("v1") {
+ *     object Users : StaticResource<Api>("users", Api) {
+ *         val list by get()
+ *             .response<List<User>>()
+ *
+ *         val create by post()
+ *             .request<UserCreation>()
+ *             .response<User>()
+ *
+ *         object User : DynamicResource<Users>("user", Users) {
+ *             val get by get()
+ *                  .response<User>()
+ *         }
+ *     }
+ * }
+ * ```
+ *
+ * An [HttpClient] can be used to call it:
+ *
+ * ```kotlin
+ * // List users:
+ * client.request(Api / Users / Users.list).bodyOrThrow()
+ *
+ * // Create a user:
+ * client.request(Api / Users / Users.create, UserCreation("John", 15)).bodyOrThrow()
+ *
+ * // Access a specific user:
+ * client.request(Api / Users / User("123456") / User.get).bodyOrThrow()
+ * ```
+ *
+ * For this example to work, you will need to configure the [HttpClient]'s `DefaultRequest` and `ContentNegotiation` plugin.
+ * To do so, please follow [our tutorial](https://spine.opensavvy.dev/setup.html#client-side-implementation).
+ *
+ * @see bodyOrNull Access the body, returning `null` on failure.
+ * @see bodyOrThrow Access the body, throwing an exception on failure.
+ * @see handle Exhaustively handle declared failures.
+ */
 suspend inline fun <reified In : Any, reified Out : Any, reified Failure : FailureSpec> HttpClient.request(
 	endpoint: ResolvedEndpoint<Endpoint<In, Out, Failure, Parameters.Empty>>,
 	input: In,
@@ -50,6 +176,48 @@ suspend inline fun <reified In : Any, reified Out : Any, reified Failure : Failu
 	crossinline configure: HttpRequestBuilder.() -> Unit = {},
 ): SpineResponse<Out, Failure> = request(endpoint, input, {}, contentType, configure)
 
+/**
+ * Invokes a [Ktor typesafe endpoint][ResolvedEndpoint].
+ *
+ * If the following API has been declared:
+ * ```kotlin
+ * object Api : RootResource("v1") {
+ *     object Users : StaticResource<Api>("users", Api) {
+ *         val list by get()
+ *             .response<List<User>>()
+ *
+ *         val create by post()
+ *             .request<UserCreation>()
+ *             .response<User>()
+ *
+ *         object User : DynamicResource<Users>("user", Users) {
+ *             val get by get()
+ *                  .response<User>()
+ *         }
+ *     }
+ * }
+ * ```
+ *
+ * An [HttpClient] can be used to call it:
+ *
+ * ```kotlin
+ * // List users:
+ * client.request(Api / Users / Users.list).bodyOrThrow()
+ *
+ * // Create a user:
+ * client.request(Api / Users / Users.create, UserCreation("John", 15)).bodyOrThrow()
+ *
+ * // Access a specific user:
+ * client.request(Api / Users / User("123456") / User.get).bodyOrThrow()
+ * ```
+ *
+ * For this example to work, you will need to configure the [HttpClient]'s `DefaultRequest` and `ContentNegotiation` plugin.
+ * To do so, please follow [our tutorial](https://spine.opensavvy.dev/setup.html#client-side-implementation).
+ *
+ * @see bodyOrNull Access the body, returning `null` on failure.
+ * @see bodyOrThrow Access the body, throwing an exception on failure.
+ * @see handle Exhaustively handle declared failures.
+ */
 suspend inline fun <reified Out : Any, reified Failure : FailureSpec> HttpClient.request(
 	endpoint: ResolvedEndpoint<Endpoint<Unit, Out, Failure, Parameters.Empty>>,
 	contentType: ContentType = ContentType.Application.Json,
